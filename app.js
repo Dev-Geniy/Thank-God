624e6bb553ec3499075b420f25b257708e405ce7
Developer Evgeniy <79357491+Dev-Geniy@users.noreply.github.com>
2025-03-01 12:51:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -419,3 +419,17 @@ document.body.addEventListener('click', () => {
     hideCounter();
 });
 
+// Регистрация сервис-воркера в вашем приложении (app.js)
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker зарегистрирован:', registration);
+      })
+      .catch((error) => {
+        console.log('Ошибка при регистрации Service Worker:', error);
+      });
+  });
+}
+
