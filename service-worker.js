cb3674210d816d5d03734fd753f8d9055fa151d1
Developer Evgeniy <79357491+Dev-Geniy@users.noreply.github.com>
2025-03-01 12:49:26 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/app.js',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png',
+        // добавьте сюда другие ресурсы, которые должны кэшироваться
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
